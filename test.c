b02a72b5476f7c7ffea4117d4887b40d30b7fa84
cquark <thesego@gmail.com>
2023-04-25 12:04:42 +0900

    removed a line

--- test.c
+++ test.c
@@ -4,7 +4,6 @@
 int main()
 {
 	printf("hello world");
-	printf("it's fun to learn how to use git");
 	printf("third line");
 	printf("this line is added on github site");
 	printf("fifth line added");
